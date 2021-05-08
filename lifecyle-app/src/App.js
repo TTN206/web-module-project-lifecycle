3d00cc6cb583f48e9e8c5241e66d221e74c9477f
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-05-07 20:58:45 -0700

    Pushing the Save to Canvas

---
+++ lifecyle-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    users: [],
+    followers: []
+  }
+
+  componentDidMount() {
+
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>SHOW ME THE MONEY!</h1>
+      </div>
+      )
+  }
+
+}
+
+export default App;
---
+++ lifecyle-app/src/Components/API_Key.js
@@ -0,0 +1,38 @@
+// Holding this info to refer to later
+
+// https://api.github.com/users/ttn206
+
+export default API_Key = {
+    "login": "TTN206",
+    "id": 72887651,
+    "node_id": "MDQ6VXNlcjcyODg3NjUx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/72887651?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/TTN206",
+    "html_url": "https://github.com/TTN206",
+    "followers_url": "https://api.github.com/users/TTN206/followers",
+    "following_url": "https://api.github.com/users/TTN206/following{/other_user}",
+    "gists_url": "https://api.github.com/users/TTN206/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/TTN206/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/TTN206/subscriptions",
+    "organizations_url": "https://api.github.com/users/TTN206/orgs",
+    "repos_url": "https://api.github.com/users/TTN206/repos",
+    "events_url": "https://api.github.com/users/TTN206/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/TTN206/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Timmy Nguyen",
+    "company": "@lambdastudents",
+    "blog": "",
+    "location": "Seattle, WA",
+    "email": null,
+    "hireable": null,
+    "bio": "Lambda Student\r\nPT Full Stack\r\n\r\n",
+    "twitter_username": null,
+    "public_repos": 58,
+    "public_gists": 0,
+    "followers": 5,
+    "following": 8,
+    "created_at": "2020-10-14T18:53:22Z",
+    "updated_at": "2021-05-07T00:46:44Z"
+  }

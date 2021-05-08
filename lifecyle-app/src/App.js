61001193041882e4808d68ce63725db777f974df
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-05-08 14:02:45 -0700

    axios call for api data not rendering just yet, UserCard component made and attached to app.js

--- lifecyle-app/src/App.js
+++ lifecyle-app/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
+import UserCard from './Components/UserCard';
 
 class App extends React.Component {
 
@@ -9,13 +11,35 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    axios // Fetch the User Data
+      .get('https://api.github.com/users/ttn206')
+      .then((res)=>{ // Display the User Data
+        this.setState({
+          users: res.data
+        })
+      })
+      .catch((err)=>{
+        console.log('unable to get user info', err)
+      })
 
+    axios // Fetch the User's Followers
+      .get('https://api.github.com/users/ttn206/followers') 
+      .then((res)=>{
+        this.setState({
+          followers: res.data
+        })
+      })
+      .catch((err)=>{
+        console.log('unable to get user followers', err)
+      })
   }
+
 
   render(){
     return (
       <div>
         <h1>SHOW ME THE MONEY!</h1>
+        <UserCard />
       </div>
       )
   }
---
+++ lifecyle-app/src/Components/UserCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class UserCard extends React.Component{
+    render() {
+        return(
+            <div>
+                <h3>User card place holder</h3>
+            </div>
+        )
+    }
+}
+
+export default UserCard;
